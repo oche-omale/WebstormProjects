9a5f78ec23e6845cf49f2a0e467f73aa176b375b
oche-omale <oche.omale@gmail.com>
2016-03-02 18:24:06 +0100

    Addition of UI-Router and Angular Translations
    Addition of UI-Router and Angular Translations

---
+++ XTForm/app/scripts/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name xtformApp
+ * @description
+ * # xtformApp
+ *
+ * Main module of the application.
+ */
+
+(function(){
+
+angular
+  .module('xtformApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.router',
+    'pascalprecht.translate'
+  ]);
+
+}());
